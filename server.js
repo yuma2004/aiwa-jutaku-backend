e114e0659c056efea8d1e3f65e8e73c4d0b4a855
yuuma <your.kayu1114.3308@gmail.com>
2024-12-24 21:41:20 +0900

    CORSの設定

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ import settingsRoutes from './src/routes/settings.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
+// 環境変数から許可するオリジンを取得
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
+
+// CORSミドルウェアの設定
+app.use(cors({
+  origin: function (origin, callback) {
+    // 許可されたオリジンリストに存在するかチェック
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS policy violation: Origin not allowed'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // 許可するHTTPメソッド
+  allowedHeaders: ['Content-Type', 'Authorization'], // 許可するHTTPヘッダ
+}));
+
 // ミドルウェア設定
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,6 +35,15 @@ app.use(bodyParser.json());
 // ルーティング設定
 app.use('/api/reservations', reservationRoutes);
 app.use('/api/settings', settingsRoutes);
+
+// CORSエラーハンドリング
+app.use((err, req, res, next) => {
+  if (err instanceof cors.CorsError) {
+    res.status(403).json({ message: 'CORSエラー: アクセスが拒否されました。' });
+  } else {
+    next(err);
+  }
+});
 
 // MongoDB接続
 mongoose.connect(process.env.MONGODB_URI, {
